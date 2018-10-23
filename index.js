1cf49570d0edc19731df217161d69b399a3b382e
Rahmat Budiharso <rbudiharso@gmail.com>
2018-10-23 15:58:45 +0700

    fix directory path

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const path = require('path')
 const envdir = dir => {
   const defaultDir = dir || process.env.ENVDIR || '.env'
 
-  const files = fs.readdirSync(path.join(__dirname, defaultDir))
+  const files = fs.readdirSync(defaultDir)
   files
     .filter(file => {
-      const stat = fs.statSync(path.join(__dirname, defaultDir, file))
+      const stat = fs.statSync(path.join(defaultDir, file))
       return stat.isFile()
     })
     .forEach(file => {
-      const value = fs.readFileSync(path.join(__dirname, defaultDir, file))
+      const value = fs.readFileSync(path.join(defaultDir, file))
       process.env[file] = value.toString().trim()
     })
 }
